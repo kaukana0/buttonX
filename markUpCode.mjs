5c7a5c9ce6857cce5c2a5fe37f4e2c55ba46c3a2
robert <kaukana0@tuta.io>
2023-06-22 18:17:33 +0200

    stars

--- markUpCode.mjs
+++ markUpCode.mjs
@@ -11,10 +11,10 @@ export default class MarkUpCode {
 		return t.content
 	}
 
-  static get(symbol) {
+  static get(symbol,w=40,h=40) {
+    if(symbol.includes("Chart")) {w=70; h=70}    //TODO
     return `
-    <svg width="40px" height="40px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-      <circle id="background" fill="#cfdaf5" cx="256" cy="256" r="256" />
+    <svg width="${w}px" height="${w}px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" tabindex="0">
       ${this.getSymbol(symbol)}
     </svg>
     ${this.getCSS()}
@@ -26,11 +26,18 @@ export default class MarkUpCode {
     symbols["close"] = this.getSymbolClose()
     symbols["lineChart"] = this.getSymbolLineChart()
     symbols["barChart"] = this.getSymbolBarChart()
+    symbols["star"] = this.getSymbolStar()
+    symbols["starFilled"] = this.getSymbolStarFilled()
     return symbols[symbol]
+  }
+
+  static circle() {
+    return `<circle id="background" fill="#cfdaf5" cx="256" cy="256" r="256" />`
   }
 
   static getSymbolClose() {
     return `
+    ${this.circle()}
     <g transform="scale(0.6, 0.6) translate(230 180)">
       <path id="symbol" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" fill="#0e47cb" stroke-width="0"/>
     </g>
@@ -39,6 +46,7 @@ export default class MarkUpCode {
 
   static getSymbolLineChart() {
     return `
+    ${this.circle()}
     <g transform="scale(0.6, 0.6) translate(210 180)">
       <path id="symbol" d="M496 384H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v336c0 17.67 14.33 32 32 32h464c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16zM464 96H345.94c-21.38 0-32.09 25.85-16.97 40.97l32.4 32.4L288 242.75l-73.37-73.37c-12.5-12.5-32.76-12.5-45.25 0l-68.69 68.69c-6.25 6.25-6.25 16.38 0 22.63l22.62 22.62c6.25 6.25 16.38 6.25 22.63 0L192 237.25l73.37 73.37c12.5 12.5 32.76 12.5 45.25 0l96-96 32.4 32.4c15.12 15.12 40.97 4.41 40.97-16.97V112c.01-8.84-7.15-16-15.99-16z" fill="#0e47cb" stroke-width="0"/>
     </g>
@@ -47,11 +55,40 @@ export default class MarkUpCode {
 
   static getSymbolBarChart() {
     return `
+    ${this.circle()}
     <g transform="scale(0.6, 0.6) translate(210 180)">
       <path id="symbol" d="M332.8 320h38.4c6.4 0 12.8-6.4 12.8-12.8V172.8c0-6.4-6.4-12.8-12.8-12.8h-38.4c-6.4 0-12.8 6.4-12.8 12.8v134.4c0 6.4 6.4 12.8 12.8 12.8zm96 0h38.4c6.4 0 12.8-6.4 12.8-12.8V76.8c0-6.4-6.4-12.8-12.8-12.8h-38.4c-6.4 0-12.8 6.4-12.8 12.8v230.4c0 6.4 6.4 12.8 12.8 12.8zm-288 0h38.4c6.4 0 12.8-6.4 12.8-12.8v-70.4c0-6.4-6.4-12.8-12.8-12.8h-38.4c-6.4 0-12.8 6.4-12.8 12.8v70.4c0 6.4 6.4 12.8 12.8 12.8zm96 0h38.4c6.4 0 12.8-6.4 12.8-12.8V108.8c0-6.4-6.4-12.8-12.8-12.8h-38.4c-6.4 0-12.8 6.4-12.8 12.8v198.4c0 6.4 6.4 12.8 12.8 12.8zM496 384H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v336c0 17.67 14.33 32 32 32h464c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16z" fill="#0e47cb" stroke-width="0"/>
     </g>
     `
   }
+
+  static getSymbolBarChart() {
+    return `
+    ${this.circle()}
+    <g transform="scale(0.6, 0.6) translate(210 180)">
+      <path id="symbol" d="M332.8 320h38.4c6.4 0 12.8-6.4 12.8-12.8V172.8c0-6.4-6.4-12.8-12.8-12.8h-38.4c-6.4 0-12.8 6.4-12.8 12.8v134.4c0 6.4 6.4 12.8 12.8 12.8zm96 0h38.4c6.4 0 12.8-6.4 12.8-12.8V76.8c0-6.4-6.4-12.8-12.8-12.8h-38.4c-6.4 0-12.8 6.4-12.8 12.8v230.4c0 6.4 6.4 12.8 12.8 12.8zm-288 0h38.4c6.4 0 12.8-6.4 12.8-12.8v-70.4c0-6.4-6.4-12.8-12.8-12.8h-38.4c-6.4 0-12.8 6.4-12.8 12.8v70.4c0 6.4 6.4 12.8 12.8 12.8zm96 0h38.4c6.4 0 12.8-6.4 12.8-12.8V108.8c0-6.4-6.4-12.8-12.8-12.8h-38.4c-6.4 0-12.8 6.4-12.8 12.8v198.4c0 6.4 6.4 12.8 12.8 12.8zM496 384H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v336c0 17.67 14.33 32 32 32h464c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16z" fill="#0e47cb" stroke-width="0"/>
+    </g>
+    `
+  }
+
+  static getSymbolStarFilled() {
+      return `
+    <g transform="scale(0.6, 0.6) translate(210 180)">
+      <path id="symbol" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" fill="#0e47cb" stroke-width="0"/>
+    </g>
+    `
+  }
+
+  static getSymbolStar() {
+    return `
+    <g transform="scale(0.6, 0.6) translate(210 180)">
+      <path id="symbol" d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z" fill="#0e47cb" stroke-width="0"/>
+    </g>
+    `
+  }
+
+
+  
 
   static getCSS() {
     return `
